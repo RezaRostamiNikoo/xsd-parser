bbd99f6067b3330f51f1aa3c2bcec28896a16043
RezaRostaminikoo <RezaRostaminikoo@gmail.com>
2023-10-27 13:45:13 -0400

    add .gitignore file for result files; updates test file

--- test/xsTree.spec.ts
+++ test/xsTree.spec.ts
@@ -1,54 +1,9 @@
-import { XsTree } from '../src'
+import { Parser, XsTree } from '../src'
 import { readFileSync } from 'fs'
 import { DOMParser } from '@xmldom/xmldom'
 import { writeFileSync } from 'fs'
 import { ITsEnumSchema } from '../src/typescriptDefinitions'
 
-export class Converter {
-
-    private _text: string
-    private _tree: xsdParser.XsTree
-
-    get tree(): xsdParser.XsTree { return this._tree }
-    /**
-     * 
-     * @param {string} ifc_xsd IFC XSD file as string
-     */
-    constructor(ifc_xsd: string) {
-
-        this._text = ifc_xsd
-
-        this._tree = new xsdParser.Parser(this._text).parse()
-
-
-    }
-
-    exportSimpleTypes(): string {
-
-        const result: Array<string> = []
-
-        const sts = this.tree.getAllInstance("xs:simpleType").filter(s => s.attribute.name) as Array<xsdParser.XsSimpleTypeNode>
-
-        sts.forEach(sn => {
-            const schema = sn.getTsSchema()
-
-            if (schema.type === "enum") {
-                const def: ITsEnumSchema = schema.definition as ITsEnumSchema
-                if (def.usage === "definition") {
-                    const t = new EnumGenerator(def.reference)
-                    t.Modifiers.export()
-                    def.items.forEach(e => t.addMember(e.toUpperCase().replace(/-/, '_')))
-                    result.push(new Writer(t.generate()).print())
-
-                }
-            }
-        })
-
-        return result.join('\n\n')
-
-    }
-
-}
 
 describe("XsTree Test File", () => {
 
